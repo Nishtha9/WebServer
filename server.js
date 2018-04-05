a66f5e953a4b6257083d69c78b593d66860fb131
Nishtha9 <Nish0349@gmail.com>
2018-04-05 19:56:48 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express=require('express');
+const hbs=require('hbs');
+
+var app=express();
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',(req,res)=>{
+// res.send('<h1>Hello Express!</h1>');
+res.render('home.hbs',{
+  pageTitle:'Home Page',
+  WelcomeMsg:'Welcome to our website!',
+  currYear: new Date().getFullYear()
+});
+});
+
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:'About Page',
+    currYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req,res)=>{
+  res.send({
+    errorMsg: "Request could not be fulfilled",
+    status: 'error'
+  });
+});
+
+app.listen(3000, ()=>{
+  console.log('Server is up on port 3000');
+});
